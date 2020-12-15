16cf619d055e9fbdbbba53c6aa0738abb812a67a
michaelrizzo2 <m_rizzo@live.com>
2020-12-14 20:07:55 -0500

    Added Backup files for project.
    Added backup files

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a37f624c4891bc91a8babf84d50e79eb.js"
+  "/precache-manifest.8055a60dc6c2bf28b54f4c3c8b4822ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
